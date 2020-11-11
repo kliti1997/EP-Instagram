3b23f4ffdde8945fd077a408db921b1c44d2878c
Timo <timokubera@yahoo.com>
2020-11-11 06:26:46 +0100

    error logging, selenium options

--- data/config.py
+++ data/config.py
@@ -6,11 +6,15 @@ from collections import defaultdict
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import os
-
-this_path = os.getcwd()
+import logging
 
 # Selenium Einsetllungen
-geckodriver = urljoin(this_path, "geckodriver")
+geckodriver = os.path.join(os.getcwd(), "geckodriver")
 driver = webdriver.Firefox(executable_path = geckodriver)
-url = "https://www.instagram.com/jimmyfallon/"
-driver.get(url)
+
+# Logger um error und info messages zu speichern
+logger = logging.getLogger("myLogger")
+f_handler = logging.FileHandler(os.path.join(os.getcwd(), "exception.log"))
+f_format = logging.Formatter('%(asctime)s - %(message)s')
+f_handler.setFormatter(f_format)
+logger.addHandler(f_handler)
