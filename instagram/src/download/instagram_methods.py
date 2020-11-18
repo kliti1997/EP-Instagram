84a4290b6acc8f56f4db7fa641232a35a33b255b
Marlon Tiedemann <marlon.tiedemann@web.de>
2020-11-18 02:07:45 +0100

    Helper function improvements

--- instagram/src/download/instagram_methods.py
+++ instagram/src/download/instagram_methods.py
@@ -1,82 +1,87 @@
+import re
 from instagram.data.config import *
 from random import randint
+from selenium.common.exceptions import NoSuchElementException
+from pprint import pprint
+from lxml import etree
+
 
 # Loggt sich auf Instagram ein.
 def login(username, password):
     driver.get(login_url)
+    random_sleep(5)
     cookie_consent()
-
-    random_sleep(10)
+    random_sleep(5)
 
     driver.find_element_by_name("username").send_keys(username)
     driver.find_element_by_name("password").send_keys(password)
 
-    if driver.find_elements_by_xpath("//*[text()='Log In']"): # Existiert as Element auf deutsch?
+    try:  # Existiert as Element auf englisch?
         driver.find_element_by_xpath("//*[text()='Log In']").click()
-    else:
-        driver.find_element_by_xpath("//*[text()='Anmelden']").click()
+    except NoSuchElementException:
+        try:
+            driver.find_element_by_xpath("//*[text()='Anmelden']").click()
+        except NoSuchElementException:
+            raise RuntimeError
 
     random_sleep(10)
 
+    if "onetap" in driver.current_url:  # "Save your Login?"-Page
+        try:  # Existiert as Element auf englisch?
+            driver.find_element_by_xpath("//*[text()='Save Info']").click()
+        except NoSuchElementException:
+            try:
+                driver.find_element_by_xpath("//*[text()='Informationen speichern']").click()
+            except NoSuchElementException:
+                raise RuntimeError
+
+    random_sleep(10)
+
+
 # Ggf. das cookie consent Fenster akzeptieren.
 def cookie_consent():
-    if driver.find_element_by_xpath("//*[text()='Akzeptieren']"): # Existiert as Element auf deutsch?
+    try:  # Existiert as Element auf deutsch?
         driver.find_element_by_xpath("//*[text()='Akzeptieren']").click()
-    elif driver.find_element_by_xpath("//*[text()='Accept']"): # Existiert es auf englisch?
-        driver.find_element_by_xpath("//*[text()='Accept']").click()
+    except NoSuchElementException:  # Existiert es auf englisch?
+        try:
+            driver.find_element_by_xpath("//*[text()='Accept']").click()
+        except NoSuchElementException:
+            pass
+
 
 # Ändert die relativen links aller img, a und link tags, indem es die
 # base_url an die relativen Pfade anhängt.
-def convert_links(base_url, soup):
-    for img in soup.find_all("img", src=True):
+def convert_links(source):
+    """for img in soup.find_all("img", src=True):
         if not img["src"].startswith("http"):
             img["src"] = urljoin(base_url, img["src"])
-    
+
     for img in soup.find_all("img", srcset=True):
         if not img["srcset"].startswith("http"):
             img["srcset"] = urljoin(base_url, img["srcset"])
-    
+
     for a in soup.find_all("a"):
         if not a["href"].startswith("http"):
             a["href"] = urljoin(base_url, a["href"])
-    
+
     for link in soup.find_all("link"):
         if not link["href"].startswith("http"):
-            link["href"] = urljoin(base_url, link["href"])
-    #TODO Scripts mit enbinden. Vgl. Marlons .html-Datei
-    """ 
-    for script in soup.find_all("script", src=True):
-        if not script["src"].startswith("http"):
-            script["src"] = urljoin(base_url, script["src"])
-    # Ändere die relativen Pfade im Source-Code der <script> Tags.
-    for script in soup.find_all("script"):
-        if script.contents: # not empty?
-            find_js = re.findall("/[\w./]*js", script.contents[0])
-            find_css = re.findall("/[\w./]*css", script.contents[0])
-            script_tmp = script.contents[0]
-            for js in find_js:
-                script_tmp = script_tmp.replace(js, base_url + js)
-                print("js")
-            for css in find_css:
-                script_tmp = script_tmp.replace(css, base_url + css)
-                print("css")
-            # Änderungen in der soup-Variable, statt in lokaler Kopie, vornehmen
-            print(script_tmp)
-            print()
-            script.string.replace_with(script_tmp)
-    """
+            link["href"] = urljoin(base_url, link["href"])"""
+
+    return re.sub(r'(href="|src="|srcset="|:")/', r'\1' + base_url, source)
+
+
 # Lade den Html-Code der Beiträge-Seite herunter.
 def html_posts(url):
     driver.get(url["href"])
     random_sleep(10)
-    content = driver.page_source
-    soup = BeautifulSoup(content, features="html.parser")
+    content = convert_links(driver.execute_script("return document.documentElement.outerHTML"))
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.fromstring(content, etree.HTMLParser())
 
-    convert_links(base_url=base_url, soup=soup)
+    with open(os.path.join(monitoring_folder, url["monitoring_folder"], "posts.html"), "w") as f:
+        f.write(content)
 
-    f = open(os.path.join(url["monitoring_folder"], "posts.html"), "w")
-    f.write(soup.prettify())
-    f.close()
 
 # Lade den Html-Code der Beiträge-Seite herunter.
 def html_igtv(url):
@@ -85,11 +90,12 @@ def html_igtv(url):
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
 
-    convert_links(base_url=base_url, soup=soup)
+    convert_links(soup=soup)
 
-    f = open(os.path.join(url["monitoring_folder"], "igtv.html"), "w")
+    f = open(os.path.join(monitoring_folder, url["monitoring_folder"], "igtv.html"), "w")
     f.write(soup.prettify())
     f.close()
+
 
 # Lädt den Html-Code der Markiert-Seite herunter
 def html_tagged(url):
@@ -98,9 +104,9 @@ def html_tagged(url):
     content = driver.page_source
     soup = BeautifulSoup(content, features="html.parser")
 
-    convert_links(base_url=base_url, soup=soup)
+    convert_links(soup=soup)
 
-    f = open(os.path.join(url["monitoring_folder"], "tagged.html"), "w")
+    f = open(os.path.join(monitoring_folder, url["monitoring_folder"], "tagged.html"), "w")
     f.write(soup.prettify())
     f.close()
 
