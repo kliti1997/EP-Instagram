2866ce19ccdca307ee011d67f4680a00c1b88099
Timo <timokubera@yahoo.com>
2020-11-25 09:00:21 +0100

    Error logging

--- instagram/src/download/instagram_store.py
+++ instagram/src/download/instagram_store.py
@@ -4,12 +4,18 @@ from instagram.src.download.instagram_methods import login, random_sleep, pre_do
 class InstagramStore:
     def __init__(self, monitoring_map):
         print("\n\n\t************STORING PHASE************")
-        login(ig_credentials["user"], ig_credentials["pass"])
+        try:
+            login(ig_credentials["user"], ig_credentials["pass"])
+        except Exception as e:
+            logger.error("performing the login process.\nException message: " + str(e))
         for url in monitoring_map["instagram"]:
             print("store the html code of : (" + url["href"] + ") in " + url["monitoring_folder"] + "old.html")
             print("OR in " + url["monitoring_folder"] + "new.html")
             print("--------------------------------------------\n")
             
-            save_html(url)
+            try:
+                save_html(url)
+            except Exception as e:
+                logger.error("downloading the html files.\nException message: " + str(e))
             random_sleep(10)
         driver.quit()
