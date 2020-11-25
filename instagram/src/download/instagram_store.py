f3823ebab946092a5b7b52df0e212b579f78b992
Kliti Nikollau <klitinikollau@hotmail.com>
2020-11-25 15:15:09 +0100

    Merge branch 'dev' of /home/linuxmint/Desktop/SWP/EP-Instagramm-1 with conflicts.

--- instagram/src/download/instagram_store.py
+++ instagram/src/download/instagram_store.py
@@ -1,30 +1,26 @@
-"""
-The module containts the class InstagramStore, which performs the download of different instagram
-subpages. 
-The "posts", "tagged" and the "IGTV" subpages will be downloaded and saved to a html file for further 
-investigation.
-"""
 from instagram.data.config import *
-from instagram.src.download.instagram_methods import login, random_sleep, pre_download, save_html
+from instagram.src.download.instagram_methods import login, random_sleep, pre_download, save_html, compare_posts
 
 class InstagramStore:
     def __init__(self, monitoring_map):
         print("\n\n\t************STORING PHASE************")
-
-        try:
-            login(ig_credentials["user"], ig_credentials["pass"])
-        except Exception as e:
-            logger.error("performing the login process.\nException message: " + str(e))
-
+        login(ig_credentials["user"], ig_credentials["pass"])
         for url in monitoring_map["instagram"]:
             print("store the html code of : (" + url["href"] + ") in " + url["monitoring_folder"] + "old.html")
             print("OR in " + url["monitoring_folder"] + "new.html")
             print("--------------------------------------------\n")
             
-            try:
-                save_html(url)
-            except Exception as e:
-                logger.error("downloading the html files.\nException message: " + str(e))
-            random_sleep(10)
+            save_html(url)
 
-        driver.quit()
+
+
+            """
+            html_posts(url)
+            random_sleep(10)
+            html_igtv(url)
+            random_sleep(10)
+            html_tagged(url)
+            """
+            driver.quit()
+            random_sleep(10)
+            #compare_posts()
