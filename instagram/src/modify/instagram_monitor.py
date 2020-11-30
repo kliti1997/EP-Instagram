c489f21cdb8f3ca07b00986c0677fa9a42a609de
Your Name <you@example.com>
2020-11-30 07:29:13 -0500

    Habe den FOR-Loop hinzugefuegt, der jedes subdirectory einzeln betrachtet

--- instagram/src/modify/instagram_monitor.py
+++ instagram/src/modify/instagram_monitor.py
@@ -8,30 +8,30 @@ class InstagramMonitor:
         for url in monitoring_map["instagram"]:
             dirname = "instagram/data/files/"+url["id"]
             #TODO name error fixen (subdirectory -> subdirectories)
-            """
+            #"""
             subdirectories = [x[0] for x in os.walk(dirname) if x[0] != dirname]
-
-            oldExist = os.path.isfile(subdirectory+"/old.html")
-            newExist = os.path.isfile(subdirectory+"/new.html")
+            for subdirectory in subdirectories:
+                oldExist = os.path.isfile(subdirectory+"/old.html")
+                newExist = os.path.isfile(subdirectory+"/new.html")
  
-            #Falls wir keine 2 Dateien haben muessen wir uns noch etwas ueberlegen
-            #Z.B. wenn wir ein neuen User laden und erst nur einmal sein Profil gecheckt haben
-            if not oldExist or not newExist:
-                return 1
+                #Falls wir keine 2 Dateien haben muessen wir uns noch etwas ueberlegen
+                #Z.B. wenn wir ein neuen User laden und erst nur einmal sein Profil gecheckt haben
+                if not oldExist or not newExist:
+                    return 1
 
-            print("compare ("+url["monitoring_folder"]+"old.html)"+" with ("+url["monitoring_folder"]+"new.html)")
+                print("compare ("+url["monitoring_folder"]+"old.html)"+" with ("+url["monitoring_folder"]+"new.html)")
             
-            oldHtml = open(subdirectory+"/old.html", "r").read()
-            oldHtml = html.unescape(oldHtml)
+                oldHtml = open(subdirectory+"/old.html", "r").read()
+                oldHtml = html.unescape(oldHtml)
 
-            newHtml = open(subdirectory+"/new.html", "r").read()
-            newHtml = html.unescape(newHtml)
+                newHtml = open(subdirectory+"/new.html", "r").read()
+                newHtml = html.unescape(newHtml)
             
-            #Verstehe nicht so recht warum die Post-Vergleichfunktion nicht hier aufgerufen wird
-            #Warum wird sie direkt in der Definitionsdatei aufgerufen?
+                #Verstehe nicht so recht warum die Post-Vergleichfunktion nicht hier aufgerufen wird
+                #Warum wird sie direkt in der Definitionsdatei aufgerufen?
             
-            compare_followers_following(oldHtml, newHtml)
-            """
+                compare_followers_following(oldHtml, newHtml)
+                #"""
 
             #TODO entfernen, nachdem name errors oben gefixt worden sind
             try:
