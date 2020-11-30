21ec63f540aab99fcbcd4a2859cf703c851c3cf8
Marlon Tiedemann <marlon.tiedemann@web.de>
2020-11-30 14:45:43 +0100

    English cookie banner clickable

--- instagram/src/download/instagram_methods.py
+++ instagram/src/download/instagram_methods.py
@@ -35,7 +35,7 @@ def login(username, password):
     """
     driver.get(base_url)
     random_sleep(5)
-    if driver.find_elements_by_xpath("//*[contains(., 'Cookies')]"):
+    if driver.find_elements_by_xpath("//*[contains(., 'Cookies') or contains(., 'cookies')]"):
         cookie_consent()
         random_sleep(5)
 
