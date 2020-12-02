5ef9b10ccfb9893e6b73756fe0ecdd40e2a0aa8a
Your Name <you@example.com>
2020-12-02 16:34:18 -0500

    Habe den Monitor umstrukturiert(genaueres als Kommentar im Source code) und die Vergleichsfunktion funktionsfaehig gemacht sowie die absolute Follower Anzahl eingefuegt

--- instagram/src/modify/instagram_monitor.py
+++ instagram/src/modify/instagram_monitor.py
@@ -1,48 +1,32 @@
 from instagram.data.config import *
 from instagram.src.modify.modify_methods import compare_posts, compare_followers_following, compare_igtv
-
-
-# from instagram.src.instagram import Instagram
+#from instagram.src.instagram import Instagram
 
 class InstagramMonitor:
     def __init__(self, monitoring_map):
         print("\n\n\t********MONITORING PHASE********")
+        #Wir untersuchen nur noch die subdirectories fuer jede einzelne Anfrage:
+        #Bspweise vergleichen wir nur die Posts Versionen miteinander, wenn auch der type Posts ist
+        #Sonst kommt es zu mehrfachen Vergleichen, weil fuer jeden einzelne url('type=posts', 'type=igtv', 'type=tagged')
+        #jedes mal alles durchgegangen wird(3*3=9 mal)
         for url in monitoring_map["instagram"]:
-            dirname = "instagram/data/files/" + url["id"]
-            # TODO name error fixen (subdirectory -> subdirectories)
-            # """
-            subdirectories = [x[0] for x in os.walk(dirname) if x[0] != dirname]
-            for subdirectory in subdirectories:
-                oldExist = os.path.isfile(subdirectory + "/old.html")
-                newExist = os.path.isfile(subdirectory + "/new.html")
+            dirname = "./instagram/data/files/"+url["id"]+"/"+url["type"]
+                
+            oldExist = os.path.isfile(dirname+"/old.html")
+            newExist = os.path.isfile(dirname+"/new.html")
+ 
+            #Falls wir keine 2 Dateien haben muessen wir uns noch etwas ueberlegen
+            #Z.B. wenn wir ein neuen User laden und erst nur einmal sein Profil gecheckt haben
+            if not oldExist or not newExist:
+                return
 
-                # Falls wir keine 2 Dateien haben muessen wir uns noch etwas ueberlegen
-                # Z.B. wenn wir ein neuen User laden und erst nur einmal sein Profil gecheckt haben
-                if not oldExist or not newExist:
-                    return
-
-                print("compare (" + url["monitoring_folder"] + "old.html)" + " with (" + url["monitoring_folder"] + "new.html)")
-
-                oldHtml = open(subdirectory + "/old.html", "r").read()
-
-                newHtml = open(subdirectory + "/new.html", "r").read()
-
-                # Verstehe nicht so recht warum die Post-Vergleichfunktion nicht hier aufgerufen wird
-                # Warum wird sie direkt in der Definitionsdatei aufgerufen?
-
-                compare_followers_following(oldHtml, newHtml)
-                # """
-
-            # TODO entfernen, nachdem name errors oben gefixt worden sind
-            try:
-                compare_followers_following("/Users/timo/Documents/Uni/SP/EP-Instagramm-1/instagram/data/files/polizei.hannover/posts/posts_old.html",
-                                            "/Users/timo/Documents/Uni/SP/EP-Instagramm-1/instagram/data/files/polizei.hannover/posts/posts.html")
-                compare_followers_following("/Users/timo/Documents/Uni/SP/EP-Instagramm-1/instagram/data/files/polizei.hannover/igtv/igtv_old.html",
-                                            "/Users/timo/Documents/Uni/SP/EP-Instagramm-1/instagram/data/files/polizei.hannover/igtv/igtv.html")
-            except Exception as e:
-                eType = e.__class__.__name__
-                logger.error("comparing the html files.\nException message: " + eType + ": " + str(e))
-
+            print("compare ("+url["monitoring_folder"]+"old.html)"+" with ("+url["monitoring_folder"]+"new.html)")
+            
+            oldHtml = open(dirname+"/old.html", "r").read()
+            newHtml = open(dirname+"/new.html", "r").read()
+            
+            outputHtml = compare_followers_following(oldHtml, newHtml)
+            open(dirname+"/new.html", "wb").write(outputHtml)
 
 """ Circular import
 if __name__ == "__main__":
