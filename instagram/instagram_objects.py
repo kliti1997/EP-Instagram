bd6559b8bc971e0ac168332c2a5d921ea15232fe
Timo <timokubera@yahoo.com>
2020-12-10 16:09:34 +0100

    Combining old and new objects per class

--- instagram/instagram_objects.py
+++ instagram/instagram_objects.py
@@ -5,17 +5,22 @@ from lxml import etree, html
 class InstagramObjects:
     def __init__(self, url=None):
         if url is None:
-            self.followers = 0
-            self.following = 0
-            self.posts = []
-            self.igtvs = []
-            self.tags = []
+            self.followers_new = 0
+            self.followers_old = 0
+            self.following_new = 0
+            self.following_old = 0
+            self.posts_new = []
+            self.posts_old = []
+            self.igtvs_new = []
+            self.igtvs_old = []
+            self.tags_new = []
+            self.tags_old = []
         else:
-            self.followers = self.set_followers(url)
-            self.following = self.set_following(url)
-            self.posts = self.set_posts(url)
-            self.igtvs = self.set_igtvs(url)
-            self.tags = self.set_tags(url)
+            self.set_followers(url)
+            self.set_following(url)
+            self.set_posts(url)
+            self.set_igtvs(url)
+            self.set_tags(url)
 
     def get_followers(self):
         return self.followers
@@ -32,17 +37,17 @@ class InstagramObjects:
     def get_tags(self):
         return self.tags
 
-    def set_followers(self):
+    def set_followers(self, url):
         pass
 
-    def set_following(self):
+    def set_following(self, url):
         pass
 
-    def set_posts(self):
+    def set_posts(self, url):
         pass
 
-    def set_igtvs(self):
+    def set_igtvs(self, url):
         pass
 
-    def set_tags(self):
+    def set_tags(self, url):
         pass
