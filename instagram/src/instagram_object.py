0c1d42af499cd2b3cdc594da16c631ef0473a6b1
Timo <timokubera@yahoo.com>
2020-12-15 19:11:11 +0100

    Adding documentation

--- instagram/src/instagram_object.py
+++ instagram/src/instagram_object.py
@@ -27,18 +27,79 @@ class InstagramObject:
         self.__set_tags(self)
 
     def get_type(self):
+        """
+        The type specifies if dom objects of a new, or old html
+        file are stored in the current isntance.
+
+        Returns:
+            int: 0 if objects of a new file are stored, otherwise 1.
+        """
         return self.type
 
     def get_tree(self):
+        """
+        The dom representation of the html file to inspect.
+
+        Returns:
+            etree: The dom tree of the whole html file.
+        """
         return self.trees
 
     def get_followers(self):
+        """
+        The method returns the parent node, which is an a-tag, of the followers count.
+        The followers count is the text attribute of the first and only child element, 
+        which is a span-tag.
+
+        Example:
+        <a class="-nal3 " href="/polizei.hannover/followers/" tabindex="0">
+            <span class="g47SY " title="28,324">28.3k</span> 
+            followers
+        </a>
+
+        Returns:
+            etree: The parent node which includes the followers count.
+        """
         return self.followers
 
     def get_following(self):
+        """
+        The method returns the parent node, which is an a-tag, of the followers count.
+        The followers count is the text attribute of the first and only child element, 
+        which is a span-tag.
+
+        Example:
+        <a class="-nal3 " href="/polizei.hannover/following/" tabindex="0">
+            <span class="g47SY ">122</span> 
+            following
+        </a>
+
+        Returns:
+            etree: The parent node which includes the following count.
+        """
         return self.following
 
     def get_posts(self):
+        """
+        The method returns a list which contains all of the 24 posts, whihc are included
+        in the dom tree. 
+        We set the first a-tag element as the root element of the post, because it contains
+        the most important data like the unique href-attribute, which is referring to the
+        thumbnail of the post.
+
+        Example:
+        <a href="/p/CInvvcLqYWw/" tabindex="0">
+            <div class="eLAPa">
+                <div class="KL4Bh">
+                    <img alt="Photo by Polizei Hannover " class="FFVAD" ...>
+                </div>
+                <div class="_9AhH0"></div>
+            </div>
+        </a>
+
+        Returns:
+            list: The list contains etree-elements, each element is a post.
+        """
         return self.posts
 
     def get_igtvs(self):
