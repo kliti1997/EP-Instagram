8cd2685152250fce36666b4916b7b2a13cdf41fc
Marlon Tiedemann <marlon.tiedemann@web.de>
2020-12-16 13:25:12 +0100

    Fixed ProfileData class

--- instagram/src/download/profile_data.py
+++ instagram/src/download/profile_data.py
@@ -2,28 +2,26 @@ import json
 
 
 class ProfileData:
-    #TODO: Exceptions fangen, aufhübschen
     """
     Class to wrap all profile data.
     """
-    num_followers = -1
-    num_following = -1
-    story_timestamp = -1
-    nodes = []  # Beinhaltet Posts, IGTV's, Markiert
-    """
-    Node structure:
-      type (str): 'image' or 'video'
-      id (str): alphabetical URL id
-      comments (int): number of comments
-      likes (int): number of likes
-      view_count (int): number of views (for videos only)
-    """
-
     def __init__(self, initial_data, requests):
+        self.num_followers = -1
+        self.num_following = -1
+        self.story_timestamp = -1
+        self.nodes = []  # Beinhaltet Posts, IGTV's, Markiert
+        """
+        Node structure:
+            type (str): 'image' or 'video'
+            id (str): alphabetical URL id
+            comments (int): number of comments
+            likes (int): number of likes
+            view_count (int): number of views (for videos only)
+        """
+
         self._initial = initial_data
         self._user = self._initial["entry_data"]["ProfilePage"][0]["graphql"]["user"]
         self._requests = requests
-
         self.read_followers()
         self.read_following()
         self.read_story()
@@ -43,14 +41,18 @@ class ProfileData:
         self.num_followers = self._user["edge_followed_by"]["count"]
 
     def read_initial_nodes(self):
-        for edge in self._user["edge_owner_to_timeline_media"]["edges"]:
-            node = {'type': 'video' if edge["node"]["is_video"] else 'image',
-                    'id': edge["node"]["shortcode"],
-                    'comments': edge["node"]["edge_liked_by"]["count"],
-                    'likes': edge["node"]["edge_liked_by"]["count"]}
+        """
 
-            if edge["node"]["is_video"]:
-                node.update({'view_count': edge["node"]["video_view_count"]})
+        """
+        for edge in self._user["edge_owner_to_timeline_media"]["edges"]:
+            child = edge["node"]
+            node = {'type': 'video' if child["is_video"] else 'image',
+                    'id': child["shortcode"],
+                    'comments': child["edge_liked_by"]["count"],
+                    'likes': child["edge_liked_by"]["count"]}
+
+            if child["is_video"]:
+                node.update({'view_count': child["video_view_count"]})
             self.nodes.append(node)
 
     def read_additional_nodes(self):
@@ -58,16 +60,21 @@ class ProfileData:
             if request.response:
                 if 'graphql' in request.url:
                     reply_content = request.response.body.decode('utf-8')
-                    if 'edge_owner_to_timeline_media' in reply_content:
+                    if 'edge_owner_to_timeline_media' in reply_content and 'edge_suggested_users' not in reply_content:
                         root = json.loads(reply_content)
-                        for edge in root["data"]["user"]["edge_owner_to_timeline_media"]["edges"]:
-                            node = {'type': 'video' if edge["node"]["is_video"] else 'image',
-                                    'id': edge["node"]["shortcode"],
-                                    'comments': edge["node"]["edge_media_to_comment"]["count"],
-                                    'likes': edge["node"]["edge_media_preview_like"]["count"]}
-                            if edge["node"]["is_video"]:
-                                node.update({'view_count': edge["node"]["video_view_count"]})
+                        for edge in list(root["data"]["user"]["edge_owner_to_timeline_media"]["edges"]):
+                            child = edge["node"]
+                            node = {'type': 'video' if child["is_video"] else 'image',
+                                    'id': child["shortcode"],
+                                    'comments': child["edge_media_to_comment"]["count"],
+                                    'likes': child["edge_media_preview_like"]["count"]}
+                            print(2)
+                            if child["is_video"]:
+                                node.update({'view_count': child["video_view_count"]})
+                            print(3)
                             self.nodes.append(node)
+                        return
+        raise RuntimeError("No request found that contains info about 13th-24th picture")
 
     def read_story(self):
         """
@@ -78,6 +85,8 @@ class ProfileData:
             if request.response:
                 if 'graphql' in request.url:
                     reply_content = request.response.body.decode('utf-8')
-                    if 'latest_reel_media' in reply_content and 'edge_suggested_users' not in reply_content:  # Identifying correct request
+                    if 'latest_reel_media' in reply_content and 'edge_suggested_users' not in reply_content:
                         reply_json = json.loads(reply_content)
                         self.story_timestamp = reply_json['data']['user']['reel']['latest_reel_media']
+                        return
+        raise RuntimeError("No request found that contains info about stories of the given user")
