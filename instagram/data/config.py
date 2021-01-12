5adf79ff984299635dd8c2634ff8890d925fb468
Dustin <dustin.erdner@web.de>
2021-01-12 02:07:55 +0100

    readded headless mode

--- instagram/data/config.py
+++ instagram/data/config.py
@@ -34,9 +34,9 @@ options = Options()
 options.headless = True
 driver_profile = webdriver.FirefoxProfile(profile_folder)
 driver_profile.set_preference('intl.accept_languages','de')
-#driver = webdriver.Firefox(firefox_profile=driver_profile, executable_path = geckodriver, log_path=geckodriver_log)
+driver = webdriver.Firefox(firefox_profile=driver_profile, executable_path = geckodriver, log_path=geckodriver_log, options=options)
 #driver = webdriver.Firefox(executable_path = os.path.join(config_folder, "geckodriver_macOS"))
-driver = webdriver.Firefox(executable_path = os.path.join(config_folder, "geckodriver"))
+#driver = webdriver.Firefox(executable_path = os.path.join(config_folder, "geckodriver"))
 """
 Seleniumwire driver and it's options.
 """
