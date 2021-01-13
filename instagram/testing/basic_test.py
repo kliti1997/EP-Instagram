f4217bba6ed370ba56b516ddac25e21ff32b2f59
Dustin <dustin.erdner@web.de>
2021-01-13 10:49:40 +0100

    print return values in basic test added

--- instagram/testing/basic_test.py
+++ instagram/testing/basic_test.py
@@ -8,24 +8,36 @@ logger = logging.getLogger('basic_test')
 monitoring_map = defaultdict(list)
 url1 = {"id": "polizei.hannover",
         "href": "https://www.instagram.com/polizei.hannover/",
-        #"type": "posts", "mode": "1", "monitoring_folder": "./polizei.hannover/posts/",
-        "type": "posts", "mode": "1", "monitoring_folder": "polizei.hannover/posts/",
+        "type": "posts", "mode": "2", "monitoring_folder": "polizei.hannover/posts/",
         "change": "", "notify": "", "err": ""}      
 url2 = {"id": "polizei.hannover",
         "href": "https://www.instagram.com/polizei.hannover/channel/",
         "type": "igtv", "mode": "1", "monitoring_folder": "polizei.hannover/igtv/",
-        "change": "", "notify": "", "err": ""}
-        
+        "change": "", "notify": "", "err": ""}        
 url3 = {"id": "polizei.hannover",
         "href": "https://www.instagram.com/polizei.hannover/tagged/",
         "type": "tagged", "mode": "1", "monitoring_folder": "polizei.hannover/tagged/",
+        "change": "", "notify": "", "err": ""}
+url4 = {"id": "frieda_rudi_make_oink",
+        "href": "https://www.instagram.com/frieda_rudi_make_oink/",
+        "type": "posts", "mode": "1", "monitoring_folder": "frieda_rudi_make_oink/posts/",
         "change": "", "notify": "", "err": ""}
         
 monitoring_map["instagram"].append(url1)
 monitoring_map["instagram"].append(url2)
 monitoring_map["instagram"].append(url3)
+monitoring_map["instagram"].append(url4)
 
 logger.info("\t\t{{{BASIC TEST}}}")
 
 InstagramStore(monitoring_map)
 InstagramMonitor(monitoring_map)
+
+logger.info("\t\t{{{RESULT}}}")
+for url in monitoring_map["instagram"]:
+    logger.info("ID:\t" + url["id"])
+    logger.info("Type:\t" + url["type"])
+    logger.info("Mode:\t" + url["mode"])
+    logger.info("Change:\t" + str(url["change"]))
+    logger.info("Notify:\t" + str(url["notify"]))
+    logger.info("Error:\t" + str(url["err"]) + "\n")
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>test.log 2>&1
 for i in {1..50}
 do
-	printf "Execution %s of 50" "$i"
+	printf "Execution %s\n" "$i"
 	python3 -m instagram.testing.basic_test
 	echo "waiting 3 seconds"
 	sleep 3
