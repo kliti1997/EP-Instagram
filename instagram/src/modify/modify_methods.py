1ba15cbf5db96652bdb8c35927d33abb666cd87a
Timo <timokubera@yahoo.com>
2021-01-13 03:22:41 +0100

    Google docstring format

--- instagram/src/modify/modify_methods.py
+++ instagram/src/modify/modify_methods.py
@@ -14,16 +14,16 @@ def pre_modify(url):
 
 def compare_posts(url, ig):
     """
-        This function compares the posts of the profile and marks the new one with a green border.
+    This function compares the posts of the profile and marks the new one with a green border.
 
-        The href of each post in the old.html file are saved in the old_links_list. The same is done for
-        the new.html file in new_links_list. The lists are than compared with one another and the posts with new
-        href are marked with a green border to show that it is a new post. We than set change and notify to true.
+    The href of each post in the old.html file are saved in the old_links_list. The same is done for
+    the new.html file in new_links_list. The lists are than compared with one another and the posts with new
+    href are marked with a green border to show that it is a new post. We than set change and notify to true.
 
-        :param url: The url of the file
-        :param ig: The instagram profile
-
-        """
+    Args:
+        url (url): The url of the file
+        ig (InstagramObject): The instagram profile
+    """
     old_links_list = []  # Only holds hrefs
     for link in ig[OLD].get_posts():
         old_links_list.append(link.attrib["href"])
@@ -77,9 +77,9 @@ def compare_igtv(url, ig):
     the new.html file in new_igtv_href_list. The lists are than compared with one another and the posts with new
     href are marked with a green border to show that it is a new post. We than set change and notify to true.
 
-    :param url: The url of the file
-    :param ig: The instagram profile
-
+    Args:
+        url (url): The url of the file
+        ig (InstagramObject): The instagram profile
     """
     # Getting all links in old igtv html file
     old_igtv_href_list = []
@@ -253,15 +253,15 @@ def compare_hover_items(url, ig):
 def compare_tagged(url, ig):
     """
     This function compares the posts in the Tagged section of the profile and marks
-        the new one with a green border.
+    the new one with a green border.
 
     The href of each post in the old.html file are saved in the old_links_list. The same is done for
     the new.html file in new_links_list. The lists are than compared with one another and the posts with new
     href are marked with a green border to show that it is a new post. We than set change and notify to true.
 
-    :param url: The url of the file
-    :param ig: The instagram profile
-
+    Args:
+        url (url): The url of the file
+        ig (InstagramObject): The instagram profile
     """
     # Take the links in the old file
     old_links_list = []  # Only holds hrefs
@@ -293,9 +293,9 @@ def compare_stories(url, ig):
     newer than old_timestamp. If this is the case, the profile picture is marked with a green border.
     The change and notify are than set to true.
 
-    :param url: The url of the file
-    :param ig: The instagram profile
-
+    Args:
+        url (url): The url of the file
+        ig (InstagramObject): The instagram profile
     """
     old_timestamp = ig[OLD].get_profile_pic_modify().attrib["data-story-timestamp"]
     new_timestamp = ig[NEW].get_profile_pic_modify().attrib["data-story-timestamp"]
